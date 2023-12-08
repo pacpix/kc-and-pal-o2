b4ed28c1a8775a4062865970e065547cf92a24e9
Julia Stachofsky <julia_stachofsky@protonmail.com>
2023-12-07 20:28:38 -0800

    updated batch build file because i fucked up folder structure and accidentally deleted my work smile emoji smile emoji smile emoji

--- KC_and_Pal/kc_build.bat
+++ KC_and_Pal/kc_build.bat
@@ -1,9 +1,9 @@
-COPY kc_and_pal.asm C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Odyssey2_Dev\aswcurr\bin\kc_and_pal.asm
-CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Odyssey2_Dev\aswcurr\bin
+COPY kc_and_pal.asm C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\aswcurr\bin\kc_and_pal.asm
+CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\aswcurr\bin
 asw kc_and_pal.asm
 p2bin kc_and_pal.p kc_and_pal.bin -r 1024-3071
 COPY kc_and_pal.bin C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Dev_Resources\o2em118win\ROMS\kc_and_pal.bin
 del kc_and_pal.asm, kc_and_pal.p, kc_and_pal.bin
 CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Dev_Resources\o2em118win\
 o2em kc_and_pal.bin -svolume=1
-CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Odyssey2_Dev\KC_and_Pal
+CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\KC_and_Pal
