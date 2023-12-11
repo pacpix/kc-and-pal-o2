9532101a6b45209804f7af6ae2d61e5bd205bf4a
Julia Stachofsky <julia_stachofsky@protonmail.com>
2023-12-10 19:22:28 -0800

    file name changes

--- KC_and_Pal/kc_build.bat
+++ KC_and_Pal/kc_build.bat
@@ -1,9 +1,9 @@
-COPY kc_and_pal.asm C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\aswcurr\bin\kc_and_pal.asm
+COPY kc_and_pal.a48 C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\aswcurr\bin\kc_and_pal.a48
 CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\aswcurr\bin
-asw kc_and_pal.asm
+asw kc_and_pal.a48
 p2bin kc_and_pal.p kc_and_pal.bin -r 1024-3071
 COPY kc_and_pal.bin C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Dev_Resources\o2em118win\ROMS\kc_and_pal.bin
-del kc_and_pal.asm, kc_and_pal.p, kc_and_pal.bin
+del kc_and_pal.a48, kc_and_pal.p, kc_and_pal.bin
 CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\Dev_Resources\o2em118win\
 o2em kc_and_pal.bin -svolume=1
 CD C:\Users\stach\OneDrive\Documents\Technology\Programming\Odyssey2\kc-and-pal-o2\KC_and_Pal
